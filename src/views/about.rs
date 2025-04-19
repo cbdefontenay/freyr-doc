19362c07c891ba8a495f78c00143a8a5ccf40774
Cyprien <cbdefontenay@gmail.com>
2025-04-19 16:57:45 +0200

    Home Seite

--- src/views/about.rs
+++ src/views/about.rs
@@ -18,7 +18,7 @@ pub fn About() -> Element {
                 div { class: "mt-10",
                     BasicButton {
                         color: ButtonColor::Freyr,
-                        label: String::from({ t!("freyr_about_doc") }),
+                        label: String::from(t!("freyr_about_doc")),
                         link: ButtonUrl {
                             url: "https://docs.rs/freyr/latest/freyr/#functions".to_string(),
                         },
