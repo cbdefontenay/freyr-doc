7c35fc399fb796ae91f4b2cdfbe4d0bc75749d98
Cyprien <cbdefontenay@gmail.com>
2025-05-03 21:11:37 +0200

    neuer component hingefügt

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -4,7 +4,8 @@ mod code_block_component;
 mod dialog_component;
 mod tabs_secondary_component;
 
-pub use home_components::HomeComponents;
+pub use home_components::*;
 pub use navbar::Navigation;
 pub use code_block_component::*;
-pub use tabs_secondary_component::TabsSecondaryComponent;
+pub use dialog_component::*;
+pub use tabs_secondary_component::*;
