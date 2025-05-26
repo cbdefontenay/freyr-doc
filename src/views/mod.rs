1c390c7ddd06e6d5a0580298660c4266723b666b
Cyprien <cbdefontenay@gmail.com>
2025-05-26 14:21:06 +0200

    spinner page

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -6,6 +6,7 @@ mod tabs_page;
 mod carousel_page;
 mod accordion_page;
 mod dialog_page;
+mod spinner_page;
 
 pub use home::Home;
 pub use about::About;
@@ -15,3 +16,4 @@ pub use tabs_page::TabsPage;
 pub use carousel_page::CarouselPage;
 pub use accordion_page::AccordionPage;
 pub use dialog_page::*;
+pub use spinner_page::*;
---
+++ src/views/spinner_page.rs
@@ -0,0 +1,9 @@
+use dioxus::prelude::*;
+use freyr::prelude::*;
+
+#[component]
+pub fn TabsPage() -> Element {
+    rsx! {
+        div { "Spinner Page will go here"}
+    }
+}
